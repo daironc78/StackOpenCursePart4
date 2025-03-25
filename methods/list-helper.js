6b5dc1b1a272aec121d96d787866a40e0833fa32
Dairon <daironc78@gmail.com>
2025-03-25 09:33:01 -0500

    ajustes lint

--- methods/list-helper.js
+++ methods/list-helper.js
@@ -1,5 +1,3 @@
-const logger = require("../utils/logger");
-
 const dummy = (blogs) => {
   const result = blogs;
   return result.length === 0 ? 1 : 1;
@@ -10,7 +8,7 @@ const totalLikes = (blogs) => {
 };
 
 const favoriteBlog = (blogs) => {
-return blogs.length > 0 ? (({ title, author, likes }) => ({ title, author, likes }))(blogs.filter(blog => blog.likes === Math.max(...blogs.map(blog => blog.likes)))[0]) : {};
+  return blogs.length > 0 ? (({ title, author, likes }) => ({ title, author, likes }))(blogs.filter(blog => blog.likes === Math.max(...blogs.map(blog => blog.likes)))[0]) : {};
 };
 
 const mostBlogs = (blogs) => {
@@ -23,7 +21,7 @@ const mostLikes = (blogs) => {
   const authors = blogs.map(blog => blog.author);
   const author = authors.sort((a, b) => blogs.filter(blog => blog.author === a).reduce((sum, blog) => sum + blog.likes, 0) - blogs.filter(blog => blog.author === b).reduce((sum, blog) => sum + blog.likes, 0)).pop();
   return { author, likes: blogs.filter(blog => blog.author === author).reduce((sum, blog) => sum + blog.likes, 0) };
-}
+};
 
 module.exports = {
   dummy,
